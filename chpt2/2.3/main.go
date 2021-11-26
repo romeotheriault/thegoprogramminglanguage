0b105b034624797e47d1873085aa32edb6b3933c
Romeo Theriault <romeotheriault@gmail.com>
2021-11-25 22:34:41 -1000

    chpt2 - minor updates

--- chpt2/2.3/main.go
+++ chpt2/2.3/main.go
@@ -9,15 +9,4 @@ import (
 func main() {
 	var x uint64 = 22
 	fmt.Println(p.PopCount(x))
-	/*
-		var x uint64 = 22
-		var count int
-		for ; x > 0; x, count = x&(x-1), count+1 {
-		}
-	*/
-
-	//fmt.Println(count)
-
-	//fmt.Printf("%08b\n", 22)
-	//fmt.Printf("%08b\n", 22&(22-1))
 }
